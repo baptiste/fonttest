08129a926ce6875cffcff0dede369fce818db13d
baptiste auguie <baptiste.auguie@gmail.com>
2021-01-16 22:07:50 +1300

    sf paths

---
+++ _get_paths.R
@@ -0,0 +1,39 @@
+# setwd("~/Documents/github/sansserrure/paths/")
+
+library(xml2)
+
+lf <- list.files(pattern = '.svg', recursive = TRUE)
+names(lf) <- gsub('.svg','',lf)
+
+codify <- function(code = '1D43A'){
+  message(code)
+  
+  a <- xml2::read_xml(glue::glue('{code}.svg'))
+  # xml_child(a)
+  # get third g
+  g2 <- as_list(xml_contents(xml_children(a)[3]))
+  paths <- vapply(g2, attr,'a', "d") # extract paths
+  
+  # strip M from first path
+  paths[1] <- gsub('M ','',paths[1])
+  d <- paste(paths, collapse=' ')
+  
+  list(code=glue::glue("0x{code}"),
+       position = c(.75, 0.0, 0.55), 
+       p =  glue::glue("@d@-",.open = '@', .close= '@'))
+}
+
+all <- lapply(names(lf), codify)
+saveRDS(all, file='glyphs.rds')
+jsonlite::write_json(all, path = 'glyphs.json',
+                     pretty = TRUE, auto_unbox = TRUE)
+
+library(glue)
+
+as_string <- function(el){
+  glue("@el$code@ : [@el$position[1]@,@el$position[2]@,@el$position[3]@, {p: '@el$p@'}]", .open='@',.close='@')
+}
+
+cat(glue_collapse(lapply(all, as_string), sep=',\n', last=''), file = 'pbcopy')
+
+cat(paste0('\\',names(lf)))
